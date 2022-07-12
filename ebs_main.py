03cc58e9648a3af31959c68104b8f5583d84ed41
Titov Roman <r.titov@itmo.ru>
2022-07-12 19:06:23 +0300

    added drafts (new files for sindy)

--- ebs_main.py
+++ ebs_main.py
@@ -5,6 +5,7 @@ import torch
 from func import load_data
 from epde_general import epde_equations
 from bamt_general import bs_experiment
+from sindy_general import sindy_equations
 from solver_general import solver_equations
 from func import confidence_region as conf_plt
 
@@ -16,16 +17,17 @@ if __name__ == '__main__':
         'KdV_equation': load_data.KdV_equation
     }
 
-    title = list(tasks.keys())[0]  # name of the problem/equation
+    title = list(tasks.keys())[2]  # name of the problem/equation
 
     u, grid_u, derivatives, cfg, params, b_conds = tasks[title]()
 
-    for variance in cfg.params["glob_epde"]["variance_arr"]:
+    df_main = sindy_equations("""YOUR CODE HERE""")
+    # for variance in cfg.params["glob_epde"]["variance_arr"]:
 
-        df_main, epde_search_obj = epde_equations(u, grid_u, derivatives, cfg, variance, title)
+    # df_main, epde_search_obj = epde_equations(u, grid_u, derivatives, cfg, variance, title)
 
-        equations = bs_experiment(df_main, cfg, title)
+    # equations = bs_experiment(df_main, cfg, title)
 
-        u_main, grid_main = solver_equations(cfg, params, b_conds, equations, epde_search_obj, title)
+    # u_main, grid_main = solver_equations(cfg, params, b_conds, equations, epde_search_obj, title)
 
-        conf_plt.confidence_region_print(u, cfg, params, u_main, grid_main, variance)
+    # conf_plt.confidence_region_print(u, cfg, params, u_main, grid_main, variance)
